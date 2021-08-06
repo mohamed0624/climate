82e04af6b60299214e372e43c9d8328c256eac23
momoscore <84141773+momoscore@users.noreply.github.com>
2021-08-06 17:01:01 +0530

    Initial commit

---
+++ lib/services/location.dart
@@ -0,0 +1,19 @@
+import 'package:geolocator/geolocator.dart';
+
+
+class Location{
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
+      try{
+        Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        LocationPermission permission = await Geolocator.requestPermission();
+        latitude =position.latitude;
+        longitude=position.longitude;
+      }catch(e){
+        print(e);
+      }
+
+    }
+  }
